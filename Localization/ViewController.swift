3c10e245a4a38111d92bb8a86fdcde04c5ee9dcd
Kelvin Tan <kelvin.vins@gmail.com>
2018-12-15 11:21:18 +0800

    First commit

--- Localization/ViewController.swift
+++ Localization/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func changeLanguage(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            label.text = "hello_world".localizableString("en")
+        case 1:
+            label.text = "hello_world".localizableString("ko")
+        case 2:
+            label.text = "hello_world".localizableString("th")
+        default:
+            break
+        }
+    }
+}
 
+extension String {
+    func localizableString(_ name: String) -> String {
+        let path = Bundle.main.path(forResource: name, ofType: "lproj")
+        let bundle = Bundle(path: path!)
+        return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: "", comment: "")
+    }
 }
 
